7b9d391938f6d7944221410d118670d0b8ea1059
Mazzzei <stimorrolnice@mail.ru>
2017-07-15 15:20:16 +0600

    den 2
    konec

---
+++ kostya.kovalev/src/com/company/Animal.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by Natalya on 15.07.2017.
+ */
+public class Animal {
+    private final String name;
+
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal with name" +name;
+    }
+}
