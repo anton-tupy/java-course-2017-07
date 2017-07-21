0328d20738c87dff5fdd34b28f3a2b2060f8e203
Dmitry <bright1308@gmail.com>
2017-07-21 11:53:21 +0700

    Day2-1

--- dmitry.shepin/day2-1/src/com/company/Main.java
+++ dmitry.shepin/day2-1/src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         Calculator calculator = new Calculator();
-        calculator.run(args.length > 0 ? args[0] : null);
+        calculator.run(args.length > 0 ? args[0] : null); //dfsgsbgf
     }
 }
