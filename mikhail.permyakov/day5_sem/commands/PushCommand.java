0d00ad1501daf5cd5a9bfd66cdcb67d623b42d4f
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-07-27 19:43:51 +0700

    added calc files

---
+++ mikhail.permyakov/day5_sem/commands/PushCommand.java
@@ -0,0 +1,30 @@
+package com.company.commands;
+
+import com.company.CalculatorContext;
+import com.company.Command;
+
+import java.util.List;
+
+/**
+ * Created by IT-Academy on 20.07.2017.
+ */
+public class PushCommand implements Command {
+    @Override
+    public void execute(List<String> arguments, CalculatorContext context) {
+        if (arguments.isEmpty()) {
+            throw new RuntimeException("PushCommand: No arguments");
+        }
+
+        String arg = arguments.get(0);
+
+        float value;
+        if (context.isValue(arg)) {
+            value = context.getValue(arg);
+        }
+        else {
+            value = Float.parseFloat(arg);
+        }
+
+        context.push(value);
+    }
+}
