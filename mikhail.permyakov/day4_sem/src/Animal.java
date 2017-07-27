8d0dfe19bd6aada6731dbcccc01640b6e6fd7016
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-07-27 18:26:50 +0700

    added several files

---
+++ mikhail.permyakov/day4_sem/src/Animal.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Natalya on 22.07.2017.
+ */
+public class Animal implements Runner {
+    private int speed=120;
+
+    public void run(){
+        System.out.println("I'm running (speed = "+ speed +"km/h)");
+    }
+}
---
+++ mikhail.permyakov/day4_sem/src/Main.java
@@ -0,0 +1,41 @@
+import java.lang.reflect.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
+
+//        Class animalClass = Animal.class; //
+//        System.out.println("class name= "+ animalClass.getSimpleName());  // метод класса Class
+//        System.out.println("class full name= "+ animalClass.getName());
+
+        Class<?> animalClass2 =  Class.forName("Animal");  //загружаем класс по названию
+        System.out.println("Class2 full name= "+ animalClass2.getName());
+
+        Object obj = animalClass2.newInstance();      // объект от класса который открыли по названию
+        Class<?> objClass = obj.getClass();
+        System.out.println("Object class: "+ objClass.getName());
+
+        Method runMethod = objClass.getMethod("run");  //runMethod - объект класса Method предоставляющий доступ к методу(определенному)
+        runMethod.invoke(obj); // вызов метода
+
+//        Runner animal = (Runner) obj;
+//        animal.run();
+        Field speedField= objClass.getDeclaredField("speed");
+        speedField.setAccessible(true); // позволяет получить доступ к приватному полю
+        int speedValue= speedField.getInt(obj);
+        System.out.println("speed= " + speedValue+ "km/h");
+        speedField.setInt(obj, 20);  //поменяли значение поля speed
+        runMethod.invoke(obj);
+
+        Runner proxy= (Runner)Proxy.newProxyInstance( //1-й пар-р класс лоадер, 2-й пар-р интерфейсы которые хотим реализовать, 3-й обрабатывает вызов методов
+                Main.class.getClassLoader(), new Class[]{Runner.class}, new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                        System.out.println("Called method "+ method.getName());
+                        return null;
+                    }
+                });
+        proxy.run();
+
+    }
+}
