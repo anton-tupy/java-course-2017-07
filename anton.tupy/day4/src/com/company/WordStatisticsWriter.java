d5cec4ab60815dc92582c3900cb8911be410c597
unknown <symalex@gmail.com>
2017-07-29 16:39:31 +0700

    Day 4

---
+++ anton.tupy/day4/src/com/company/WordStatisticsWriter.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+/**
+ * Created by IT-Academy on 29.07.2017.
+ */
+public class WordStatisticsWriter {
+    public void write(String outputPath, List<WordStatisticsRecord> statistics) {
+        try (OutputStreamWriter writer
+                     = new OutputStreamWriter(new FileOutputStream(outputPath))) {
+            for (WordStatisticsRecord statistic : statistics) {
+                writer.write(String.format("%s,%s,%s",
+                        statistic.getWord(), statistic.getCount(), statistic.getPercent()));
+                writer.write("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
