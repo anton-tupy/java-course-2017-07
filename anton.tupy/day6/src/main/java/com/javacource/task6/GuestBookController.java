d52535bc55efbfd45b717a1020cbcba4406f80cb
unknown <symalex@gmail.com>
2017-08-10 22:25:54 +0700

    day 6

---
+++ anton.tupy/day6/src/main/java/com/javacource/task6/GuestBookController.java
@@ -0,0 +1,45 @@
+package com.javacource.task6;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * Created by IT-Academy on 10.08.2017.
+ */
+public class GuestBookController {
+
+    public void addMessage(String message) throws SQLException {
+        try (Connection connection = connect()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO POSTS(POSTMESSAGE, POSTDATE) VALUES(?, ?)");
+            statement.setString(1, message);
+            java.sql.Date date = new java.sql.Date(new java.util.Date().getTime());
+            statement.setDate(2, date);
+
+            statement.executeUpdate();
+        }
+    }
+
+    public List<Post> allMessages() throws SQLException {
+        try (Connection connection = connect()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT ID, POSTMESSAGE, POSTDATE FROM POSTS");
+
+            ResultSet resultSet = statement.executeQuery();
+
+            ArrayList<Post> posts = new ArrayList<>();
+            while(resultSet.next()) {
+                Post post = new Post(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3));
+                posts.add(post);
+            }
+            return posts;
+        }
+    }
+
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection("jdbc:h2:./messages.db", "sa", "");
+    }
+}
---
+++ anton.tupy/day6/src/main/java/com/javacource/task6/Main.java
@@ -0,0 +1,29 @@
+package com.javacource.task6;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by IT-Academy on 10.08.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        String command = args[0];
+        GuestBookController controller = new GuestBookController();
+        switch (command) {
+            case "add":
+                String message = args[1];
+                controller.addMessage(message);
+                break;
+            case "list":
+                List<Post> posts = controller.allMessages();
+                for (Post post : posts) {
+                    System.out.printf("id=%s\tdate=%s\tmessage=%s\n",
+                            post.getId(), post.getDate(), post.getMessage());
+                }
+                break;
+            default:
+                throw new RuntimeException("Unexpected command " + command);
+        }
+    }
+}
