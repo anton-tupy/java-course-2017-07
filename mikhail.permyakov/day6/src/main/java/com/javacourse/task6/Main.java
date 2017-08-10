917854ee5f43a269e23e8bdf91991b1b18305d39
Mikhail Permyakov <mikhailpermyakov@yahoo.com>
2017-08-10 22:25:00 +0700

    day6

---
+++ mikhail.permyakov/day6/src/main/java/com/javacourse/task6/Main.java
@@ -0,0 +1,28 @@
+package com.javacourse.task6;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+//        System.out.println("HW");
+        String command = args[0];
+        GuestBookController controller = new GuestBookController();
+        switch(command){
+            case "add":
+                String message = args[1];
+                controller.addMessage(message);
+                break;
+            case "list":
+                List<Post> posts = controller.allMessages();
+                for(Post post: posts){
+                    System.out.printf("id=%s\tdate=%s\tmessage=%s\n",
+                            post.getId(), post.getDate(), post.getMessage());
+                }
+                break;
+            default:
+                throw new RuntimeException("Unexpected command " + command);
+
+        }
+    }
+}
